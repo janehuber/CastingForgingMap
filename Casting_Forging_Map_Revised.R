934705dcbea349ef6bf50852acdb325ff834d7ed
janehuber <59630505+janehuber@users.noreply.github.com>
2023-07-20 12:35:33 -0500

    Polished map

--- Casting_Forging_Map_Revised.R
+++ Casting_Forging_Map_Revised.R
@@ -1,350 +1,170 @@
+#----- Setup -----
 
-# Load libraries
+## Load libraries
 library(tidyverse)
 library(sf)
-library(tmap) #???
-library(tmaptools) #???
-library(leafpop)
-library(mapview) #???
-library(geojsonsf)
-library(leaflet) #???
-library(leaflet.extras) #???
-library(scales)
-library(hrbrthemes) #???
-library(readxl)
-library(openxlsx)
-library(janitor)
+library(leaflet)
+library(leaflet.extras)
+library(RColorBrewer)
 
-# Set options, remove scientific notation, other options
+## Set options, remove scientific notation, other options
 options(scipen = 999, stringsAsFactors = FALSE)
-knitr::opts_chunk$set(echo = FALSE, message = FALSE,
-                      warning = FALSE, rows.print = 25)
-# tmap_mode("view")
-# tmap_options(basemaps = c("OpenStreetMap", "Esri.WorldGrayCanvas"))
+knitr::opts_chunk$set(
+  echo = FALSE,
+  message = FALSE,
+  warning = FALSE,
+  rows.print = 25
+)
 
-### set work directory ###
+## Set working directory
 
 setwd("~/Developer/CastingForgingMap")
 
-### ----- Import data -----
+# ----- Import Data -----
 map_data <- read.csv("Company_Data.csv")
 
-# Fix bad lat/long
+# ----- Transform Data -----
+## Fix bad lat/long
 map_data <- map_data %>%
-  mutate("longtitude" = case_when(
-    duns_number == "96-966-4429" ~
-      -87.720890,
-    TRUE ~
-      longtitude),
-    "latitude" = case_when(
-      duns_number == "96-966-4429" ~
-        42.028545,
-      TRUE ~
-        latitude))
+  mutate(
+    "longtitude" = case_when(duns_number == "96-966-4429" ~
+                               -87.720890,
+                             TRUE ~
+                               longtitude),
+    "latitude" = case_when(duns_number == "96-966-4429" ~
+                             42.028545,
+                           TRUE ~
+                             latitude)
+  )
 
-# Convert to spatial object
+## Convert to spatial object
 forge_sf <- st_as_sf(map_data,
                      coords = c("longtitude",
                                 "latitude"),
                      crs = 4326)
 
-# Fix CRS
+## Fix CRS
 forge_sf <- st_transform(forge_sf,
                          crs = 4326)
 
-### ----- Interactive map -----
-
-# Get layer names for search feature groups
-# map_layer_names <- forge_sf %>%
-#   st_drop_geometry() %>%
-#   tabyl(Legend_Specialization) %>%
-#   pull(Legend_Specialization)
-#
-# map_layer_names_2 <- unique(forge_sf[c("Legend_Specialization")])
-
-map_layer_names <-
-  forge_sf %>%
-  st_drop_geometry() %>%
-  pull("Legend_Specialization") %>%
-  unique() %>%
-  sort
-
-
-# encoding
+## Encoding
 Encoding(x = forge_sf$certification) <- "UTF-8"
 
-# replace all non UTF-8 character strings with an empty space
+## Replace all non UTF-8 character strings with an empty space
 forge_sf$certification <-
-  iconv( x = forge_sf$certification
-         , from = "UTF-8"
-         , to = "UTF-8"
-         , sub = "")
-
-# Final company map
-# final_map <- forge_sf %>%
-#   tm_shape() +
-#   tm_dots(col = "Legend_Specialization", alpha = 0.7,
-#           size = 0.15, border.col = "black",
-#           legend.is.portrait = TRUE,legend.show = TRUE,
-#           palette = "Set1",
-#           popup.vars = c("Company Name" = "company_name",
-#                          "DUNS #" = "duns_number",
-#                          "Region" = "region",
-#                          "Specialization" = "Legend_Specialization",
-#                          "Certification" = "certification",
-#                          "Subsidiary" = "subsidiary",
-#                          "Overseas Location" = "overseas",
-#                          "Number of Employees" = "employee_this_sites_2020",
-#                          "Sales" = "sales_volume_2020",
-#                          "Average Annual DoD Revenue" = "avg_ann_dod_total_revenue",
-#                          "Website" = "website",
-#                          "Web Contact" = "contact",
-#                          "Phone Number" = "phone_no",
-#                          "Address" = "address"))+
-#   tm_facets("Legend_Specialization", as.layers = TRUE) +
-#   tm_layout(title = "Establishments in Casting, Forging, and Related Sectors in the ILDMC Region")
-#
-# # Convert to leaflet and add search box, checkbox for all teams
-# final_map_leaflet <- tmap_leaflet(final_map) %>%
-#   # Layer control
-#   addLayersControl(baseGroups = c("OpenStreetMap",
-#                                   "Esri.WorldGrayCanvas"),
-#                    overlayGroups = map_layer_names,
-#                    position = "topleft",
-#                    options = layersControlOptions(collapsed = FALSE)) %>%
-#
-#   # Search box for companies
-#   addSearchFeatures(targetGroups = map_layer_names,
-#                     options = searchFeaturesOptions(
-#                       position = "topleft",
-#                       propertyLoc = "company_name",
-#                       zoom = 15,
-#                       openPopup = TRUE,
-#                       textPlaceholder = "Search Company Name...",
-#                       collapsed = FALSE)) %>%
-#   # Search box for address
-#   leaflet.extras::addSearchOSM()
-#
-#
-# add_circles_for_group <- function(group_name) {
-#
-#   # Want to filter for the gropu name in the columns
-#   # Then, want to return a command that says "add circles"
-#
-#
-#
-# }
+  iconv(
+    x = forge_sf$certification
+    ,
+    from = "UTF-8"
+    ,
+    to = "UTF-8"
+    ,
+    sub = ""
+  )
 
 
-# leaflet(forge_sf) %>%
-#   addTiles(group = "OpenStreetMap") %>%
-#   purrr::walk()
-#   addCircles(group = "Legend_Specialization") %>%
-#   addLayersControl(overlayGroups = map_layer_names,
-#                    position = "topleft",
-#                    options = layersControlOptions(collapsed = FALSE))
+## Create a split dataset that has sub-data sets for each specialization; allows us to map each specialization as a separate layer
+split_df_forge <- split(forge_sf, forge_sf$Legend_Specialization)
 
-install.packages("RColorBrewer")                              # Install & load RColorBrewer package
-library("RColorBrewer")
+# ----- Create Map -----
 
-# Create list of colors to be used for dots that will be displayed in the map description thing
-
+# map_layer_names <-
+#   forge_sf %>%
+#   st_drop_geometry() %>%
+#   pull("Legend_Specialization") %>%
+#   unique() %>%
+#   sort
 
 
+## Function that maps a distinct color to each Specialization
+pal <- colorFactor(palette = 'Set1',
+                   domain = names(split_df_forge))
+
+## Create the popup text (with HTML styling) for the map
+generate_popup <- function(row) {
+  popup_text <- paste0(
+    "<b>Company Name: </b>",
+    row$company_name,
+    "<br> <b>DUNS #: </b>",
+    row$duns_number,
+    "<br> <b>Region: </b>",
+    row$region,
+    "<br> <b>Specialization: </b>",
+    row$Legend_Specialization,
+    "<br> <b>Certification: </b>",
+    row$certification,
+    "<br> <b>Subsidiary: </b>",
+    row$subsidiary,
+    "<br> <b>Overseas Location: </b>",
+    row$overseas,
+    "<br> <b>Number of Employees: </b>",
+    row$employee_this_sites_2020,
+    "<br> <b>Sales: </b>",
+    row$sales_volume_2020,
+    "<br> <b>Average Annual DoD Revenue: </b>",
+    row$avg_ann_dod_total_revenue,
+    "<br> <b>Website: </b>",
+    row$website,
+    "<br> <b>Web Contact: </b>",
+    row$contact,
+    "<br> <b>Phone Number: </b>",
+    row$phone_no,
+    "<br> <b>Address: </b>",
+    row$address
+  )
+
+  popup_text
+}
+
+## Define the Leaflet map
+l <-
+  leaflet() %>%
+  addTiles()
+
+## Add a circle to the leaflet map defined above, iteratively for each specialization
+names(split_df_forge) %>%
+  purrr::walk(function(df) {
+    l <<- l %>%
+      addCircles(
+        stroke = TRUE,
+        data = split_df_forge[[df]],
+        label =  ~ as.character(company_name),
+        popup = ~ generate_popup(split_df_forge[[df]]),
+        group = df,
+        color = pal(split_df_forge[[df]]$Legend_Specialization),
+        labelOptions = labelOptions(noHide = F,
+                                    direction = 'auto')
+      )
+  })
+
+## Finally, with all of our layers added, include layer control, legend, etc.
+l <-
+  l %>%
+  addLayersControl(overlayGroups = names(split_df_forge),
+                   options = layersControlOptions(collapsed = FALSE)) %>%
+  addLegend("topright",
+            pal,
+            values = names(split_df_forge),
+            title = "Specialization") %>%
+  # Search box for companies
+  addSearchFeatures(
+    targetGroups = names(split_df_forge),
+    options = searchFeaturesOptions(
+      position = "topleft",
+      propertyLoc = "company_name",
+      zoom = 15,
+      openPopup = TRUE,
+      textPlaceholder = "Search Company Name...",
+      collapsed = FALSE
+    )
+  ) %>%
+  # Search box for address
+  leaflet.extras::addSearchOSM()
 
 
-palette3_info <- brewer.pal.info[forge_sf$Legend_Specialization == "qual", ]
+# ----- Export map and data -----
 
-scale_colour_brewer(palette = "Set1")
-
-pal <- colorFactor(
-  palette = 'Set1',
-  domain = names(split_df_forge)
-)
-
-sf_2 <- forge_sf
-
-sf_2$color <- pal(sf_2$Legend_Specialization)
-
-pal <- colorFactor(palette = "Set1", levels = levels(forge_sf$Legend_Specialization))
-
-
-  split_df_forge <- split(forge_sf, forge_sf$Legend_Specialization)
-
-
-  add_circles_to_map <- function() {
-
-    temp_data <- forge_sf
-    pal <- colorFactor(palette = "Set1", levels = levels(forge_sf$Legend_Specialization))
-    temp_data$colors <- pal(temp_data$Legend_Specialization)
-
-    head(temp_data$colors)
-  }
-
-
-  # MAJOR EXPERIMENT 1
-  l <-
-    leaflet() %>%
-    addTiles() %>%
-    addCircles(
-      data = ,
-      weight = 3,
-      radius = 5,
-      label = ~ company_name,
-      popup = ~ generate_popup(forge_sf),
-      color = ~ pal(Legend_Specialization),
-      group = "Test"
-    ) %>%
-    addLegend(
-      data = forge_sf,
-      "topright",
-      pal,
-      values =  ~ Legend_Specialization,
-      labels = names(forge_sf$Legend_Specialization),
-      title = "Specialization"
-    ) %>%
-    addLayersControl(
-      data = forge_sf,
-      overlayGroups = c("Test"),
-                     options = layersControlOptions(collapsed = FALSE)) %>%
-    # Search box for companies
-    addSearchFeatures(targetGroups = map_layer_names,
-                      options = searchFeaturesOptions(
-                        position = "topleft",
-                        propertyLoc = "company_name",
-                        zoom = 15,
-                        openPopup = TRUE,
-                        textPlaceholder = "Search Company Name...",
-                        collapsed = FALSE)) %>%
-    # Search box for address
-    leaflet.extras::addSearchOSM()
-  l
-
-  # %>%
-  #   addLegend(position = "bottomright",
-  #             colors = names(split_df_forge))
-
-  generate_popup <- function(row) {
-
-    popup_text <- paste0("<b>Company Name: </b>", row$company_name,
-                         "<br> <b>DUNS #: </b>", row$duns_number)
-
-    popup_text
-  }
-
-
-
-
-
-  # Here is where we go in and add circles for each thing... Do we want a better way to do this?
-  l <-
-    leaflet(forge_sf) %>%
-    addTiles() %>%
-    addLegend(
-      "topright",
-      pal,
-      values =  ~ Legend_Specialization,
-      labels = names(forge_sf$Legend_Specialization),
-      title = "Specialization"
-    ) %>%
-    hideGroup(group = "Test") %>%
-    addLayersControl(overlayGroups = c("Test"),
-                     options = layersControlOptions(collapsed = FALSE)) %>%
-    # Search box for companies
-    addSearchFeatures(targetGroups = map_layer_names,
-                      options = searchFeaturesOptions(
-                        position = "topleft",
-                        propertyLoc = "company_name",
-                        zoom = 15,
-                        openPopup = TRUE,
-                        textPlaceholder = "Search Company Name...",
-                        collapsed = FALSE)) %>%
-    # Search box for address
-    leaflet.extras::addSearchOSM()
-  l
-
-
-  l <-
-    leaflet(split_df_forge) %>%
-    addTiles() %>%
-    addLegend(
-      "topright",
-      pal,
-      values =  ~ names(split_df_forge),
-      labels = names(split_df_forge),
-      title = "Specialization"
-    ) %>%
-    addLayersControl(overlayGroups = names(split_df_forge),
-                     options = layersControlOptions(collapsed = FALSE)) %>%
-    # Search box for companies
-    addSearchFeatures(targetGroups = map_layer_names,
-                      options = searchFeaturesOptions(
-                        position = "topleft",
-                        propertyLoc = "company_name",
-                        zoom = 15,
-                        openPopup = TRUE,
-                        textPlaceholder = "Search Company Name...",
-                        collapsed = FALSE)) %>%
-    # Search box for address
-    leaflet.extras::addSearchOSM()
-
-  # EXPERIMENT 3
-
-  l <-
-    leaflet() %>%
-    addTiles() %>%
-    addTitle(text = "Title")
-
-    names(split_df_forge) %>%
-    purrr::walk(function(df) {
-      l <<- l %>%
-        addCircles(
-          stroke = TRUE,
-          data = split_df_forge[[df]],
-          label =  ~ as.character(company_name),
-          popup = ~ generate_popup(split_df_forge[[df]]),
-          group = df,
-          color = pal(split_df_forge[[df]]$Legend_Specialization),
-          labelOptions = labelOptions(noHide = F,
-                                      direction = 'auto')
-        )
-    })
-
-    l %>%
-      addLayersControl(
-        overlayGroups = names(split_df_forge),
-        options = layersControlOptions(collapsed = FALSE)
-      ) %>%
-      addLegend(
-        "topright",
-        pal,
-        values = c("Casting",
-                     "Die and tool",
-                     "Energy storage",
-                     "Fastener-connection-rivet-washer",
-                     "Forging",
-                     "Machining",
-                     "Metal closure",
-                     "Metal stamping",
-                     "Roll forming"),
-        title = "Specialization"
-      ) %>%
-    # Search box for companies
-    addSearchFeatures(targetGroups = map_layer_names,
-                      options = searchFeaturesOptions(
-                        position = "topleft",
-                        propertyLoc = "company_name",
-                        zoom = 15,
-                        openPopup = TRUE,
-                        textPlaceholder = "Search Company Name...",
-                        collapsed = FALSE)) %>%
-      # Search box for address
-      leaflet.extras::addSearchOSM()
-
-
-### ----- Export map and data -----
-
-# Export map
+## Export map
 mapshot(l,
         url = ("index.html"),
         title = "Interactive Map")
